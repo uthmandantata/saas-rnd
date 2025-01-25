bc700f7f9af4e3f3de4aa3c80d25f292670e4c05
Uthman Dantata <uthmandantata8@gmail.com>
2025-01-25 19:25:22 +0100

    Prepare for Railway

--- src/focalleap/settings.py
+++ src/focalleap/settings.py
@@ -25,7 +25,15 @@ SECRET_KEY = 'django-insecure-@x8m(o-zpr7-jr)%nfqi8j$3&6(32=3im*^_93sn^xsil+17&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".railway.app" # https://saas.prod.railway.app
+]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        "127.0.0.1",
+        "localhost"
+    ]
 
 
 # Application definition
--- src/focalleap/views.py
+++ src/focalleap/views.py
@@ -10,10 +10,37 @@ this_dir = pathlib.Path(__file__).resolve().parent
 def home_page_view(request, *args, **kwargs):
     qs = PageVisit.objects.all()
     page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count()
+    except:
+        percent = 0
     my_title = "Home"
     my_context = {
         "page_title": my_title,
         "page_visit_count":page_qs.count(),
+        "percent":percent,
+        "total_visit_count":qs.count()
+    }
+    path = request.path
+    html_template="home.html"
+    PageVisit.objects.create(
+        path=path
+    )
+    return render(request, html_template, my_context)
+
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisit.objects.all()
+    page_qs = PageVisit.objects.filter(path=request.path)
+    try:
+        percent = (page_qs.count() * 100.0) / qs.count(),
+    except:
+        percent = 0
+    my_title = "Home"
+    my_context = {
+        "page_title": my_title,
+        "page_visit_count":page_qs.count(),
+        "percent":percent,
         "total_visit_count":qs.count()
     }
     path = request.path
