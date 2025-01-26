d909fbf057842c8fe73f78d84c9b126df1a8b646
Uthman Dantata <uthmandantata8@gmail.com>
2025-01-26 08:58:25 +0100

    Connected to prosgres

--- src/focalleap/settings.py
+++ src/focalleap/settings.py
@@ -88,12 +88,19 @@ WSGI_APPLICATION = 'focalleap.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+CONN_MAX_AGE = config("CONN_MAX_AGE", cast=int, default=30)
+DATABASE_URL = config("DATABASE_URL", cast=str)
+
+if DATABASE_URL is not None:
+    import dj_database_url
+    DATABASES = {
+    'default': dj_database_url.config(
+        default=DATABASE_URL,
+        conn_max_age=CONN_MAX_AGE,
+        conn_health_checks=True,
+    )
     }
-}
 
 
 # Password validation
